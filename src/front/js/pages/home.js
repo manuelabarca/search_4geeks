293408022e523325ca7d231bd0c1bdfab298c35c
Manuel Labarca <labarca.manu@gmail.com>
2021-09-16 17:57:59 +0000

    searchbox

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,54 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const [users, setUsers] = useState([]);
+	const [text, setText] = useState("");
+	const [suggestions, setSuggestions] = useState([]);
+
+	useEffect(() => {
+		fetch("https://reqres.in/api/users")
+			.then(resp => {
+				if (resp.ok) {
+					return resp.json();
+				}
+			})
+			.then(json => setUsers(json.data));
+	}, []);
+
+	const onChangeHandler = text => {
+		let matches = [];
+		if (text.length > 0) {
+			matches = users.filter(user => {
+				const regex = new RegExp(`${text}`, "gi");
+				return user.email.match(regex);
+			});
+		}
+		setSuggestions(matches);
+		setText(text);
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<div className="container">
+				<input
+					type="text"
+					className="col-md-12 input"
+					onChange={e => onChangeHandler(e.target.value)}
+					value={text}
+					onBlur={() => setSuggestions([])}
+				/>
+			</div>
+
+			{suggestions &&
+				suggestions.map((suggestion, i) => (
+					<div key={i} className="suggestion col-md-12 justify-content-md-center">
+						{suggestion.email}
+					</div>
+				))}
 		</div>
 	);
 };
